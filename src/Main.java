0c82c9c35faebc763ab5801f2550757c2442030e
amirparsaro <amirparsarouhani@gmail.com>
2025-03-25 21:48:32 +0330

    fizzbuzz game but scalable

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter how many numbers you want to use a word for in a separate line: ");
+        int numAmount = scn.nextInt();
+        scn.nextLine();
+
+        int[] numbers = new int[numAmount];
+        String[] words = new String[numAmount];
+        for (int i = 0; i < numAmount; i++) {
+            System.out.print("Enter a number in a separate line: ");
+            numbers[i] = scn.nextInt();
+            scn.nextLine();
+
+            System.out.print("Enter a word for that number in a separate line: ");
+            words[i] = scn.nextLine();
+        }
+
+        System.out.print("Enter the first number in the range in a separate line: ");
+        int startNum = scn.nextInt();
+        scn.nextLine();
+
+        System.out.print("Please enter the final number for the range in a separate line: ");
+        int endNum = scn.nextInt();
+        scn.nextLine();
+
+        for (int i = startNum; i <= endNum; i++) {
             String output = "";
 
-            if (i % 3 == 0) {
-                output += "Fizz";
-            }
-            if (i % 5 == 0) {
-                output += "Buzz";
+            for (int j = 0; j < numAmount; j++) {
+                if (i % numbers[j] == 0) {
+                    output += words[j];
+                }
             }
 
             if (output.isEmpty()) {
