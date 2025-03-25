0f2be43062ed7169bc43ad80645969061b8035dd
amirparsaro <amirparsarouhani@gmail.com>
2025-03-25 21:18:44 +0330

    fizzbuzz game but not scalable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (int i = 1; i <= 100; i++) {
+            String output = "";
+
+            if (i % 3 == 0) {
+                output += "Fizz";
+            }
+            if (i % 5 == 0) {
+                output += "Buzz";
+            }
+
+            if (output.isEmpty()) {
+                output = String.valueOf(i);
+            }
+            System.out.println(output);
+        }
     }
 }
